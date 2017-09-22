a0bc03bc88a6256129c72aec1ae554e0d2750797
Vitali Mueller <vitali_mueller@yahoo.de>
2017-09-22 20:12:12 +0100

    Update data_preprocessing.py

--- Data Preprocessing/data_preprocessing.py
+++ Data Preprocessing/data_preprocessing.py
@@ -10,14 +10,33 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
+# Checking for Missing Data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy='mean', axis=0)
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+# Checking for Categorical Data
+from sklearn.preprocessing import LabelEncoder
+LabelEncoder_X = LabelEncoder()
+X[:,0] = LabelEncoder_X.fit_transform(X[:,0])
+# Need to create dummy variables for categorical data
+# Encoding categorical data
+from sklearn.preprocessing import OneHotEncoder
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
+X = oneHotEncoder.fit_transform(X).toarray()
+# Label Encoding Y values 0 and 1 for no and yes
+LabelEncoder_y = LabelEncoder()
+y = LabelEncoder_y.fit_transform(y)
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+y_train = sc_y.fit_transform(y_train)
