b4dd773cdbd5973f06fce5e602376f672c3001dc
vitaliAI <vitali_mueller@yahoo.de>
2017-09-28 20:27:46 +0100

    Decision Tree Regression

---
+++ Regression/Decision Tree Regression/decision_tree_regression.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 28 20:03:06 2017
+
+@author: vmueller
+"""
+
+# Import Libaries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Import Dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:,1:2].values
+y = dataset.iloc[:,2].values
+
+# Fitting the Decion Tree Regression
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(X,y)
+
+# Predict a new result
+y_pred = regressor.predict(6.5)
+
+
+
+
+# Visualisation the Decision Tree Regression
+plt.scatter(X, y, color='red')
+plt.plot(X, regressor.predict(X), color='blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show() 
+
+# Visualize the Regression results for higher resolution
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape(len(X_grid), 1)
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title('Truth or Bluff')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
